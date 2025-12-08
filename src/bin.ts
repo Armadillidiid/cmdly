683fc2109b4deb0f68c58fe4e2eae8974640e4b2
Emmanuel Isenah <emmanuelisenah@gmail.com>
2025-12-08 09:50:34 +0100

    setup service

--- src/bin.ts
+++ src/bin.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 
-import * as NodeContext from "@effect/platform-node/NodeContext"
-import * as NodeRuntime from "@effect/platform-node/NodeRuntime"
-import * as Effect from "effect/Effect"
-import { run } from "./cli.js"
+import * as NodeContext from "@effect/platform-node/NodeContext";
+import * as NodeRuntime from "@effect/platform-node/NodeRuntime";
+import { Effect, Layer } from "effect";
+import { CliService } from "./cli.js";
 
-run(process.argv).pipe(
-  Effect.provide(NodeContext.layer),
-  NodeRuntime.runMain({ disableErrorReporting: true })
-)
+const layers = Layer.mergeAll(CliService.Default, NodeContext.layer);
+
+Effect.gen(function* () {
+  const cli = yield* CliService;
+  yield* cli.run(process.argv);
+}).pipe(Effect.provide(layers), NodeRuntime.runMain());
--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,31 @@
 import * as Command from "@effect/cli/Command";
+import { Effect } from "effect";
 
 declare const __VERSION__: string;
 const VERSION: string =
   typeof __VERSION__ !== "undefined" ? __VERSION__ : "0.0.0-dev";
 
-const command = Command.make("cmd-sage");
+const mainCommand = Command.make("cmd-sage", {}, () =>
+  Effect.sync(() => {
+    console.log(
+      `cmd-sage v${VERSION}. run cmd-sage --help for more information.`,
+    );
+  }),
+);
+// .pipe(
+//   Command.withSubcommands([])
+// );
 
-export const run = Command.run(command, {
-  name: "cmd-sage",
-  version: VERSION,
+const cliService = Effect.gen(function* () {
+  return {
+    run: (argv: string[]) =>
+      Command.run(mainCommand, {
+        name: "cmd-sage",
+        version: VERSION,
+      })(argv),
+  };
 });
+
+export class CliService extends Effect.Service<CliService>()("CliService", {
+  effect: cliService,
+}) {}
